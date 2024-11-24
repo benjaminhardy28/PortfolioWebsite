39a706b6d69dae3c3829fcfafe7cf6c229e14ea5
Ben Hardy <107066058+benjaminhardy28@users.noreply.github.com>
2024-11-24 11:12:46 -0600

    lots of unupdated changes

--- src/components/CustomCarousel.js
+++ src/components/CustomCarousel.js
@@ -1,39 +1,24 @@
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
-
 const CustomCarousel = ({picures_data }) => {
 
 
   const space = { 
- 
-
-    // boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
-    // border: 'none',
     width: '80%',
-    // top: '1.5vh',
-    // left: '3vw',
-    // display:'flex',
-    // backgroundcolor: 'blue'
     borderRadius: '20px',
     boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
     border: 'none',
     marginLeft: '2em',
   }
-
-  //border-radius: 20px; box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px; border: none; width: 80%; top: 1.5vh; left: 3vw;
-
   return (
     <Carousel >
       {
         picures_data.map( picture => {
           return (
-            <Carousel.Item key={picture.id} style={space}>
+            <Carousel.Item key={picture.id} > 
               <img 
                 className="d-block w-100"
-                style={{width: '100%'}}
                 src={picture.image} 
                 />
             </Carousel.Item>
